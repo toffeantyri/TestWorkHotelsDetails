37e77228d0c38784c68d0829da2cc7106ede1bdd
toffeantyri <toffeantyri@gmail.com>
2023-09-05 09:08:25 +0500

    get colors, fonts, textSize from figma project.

--- app/src/main/kotlin/com/testwork/hotels/ui/first_fragment/adapter/HotelViewHolder.kt
+++ app/src/main/kotlin/com/testwork/hotels/ui/first_fragment/adapter/HotelViewHolder.kt
@@ -2,14 +2,25 @@ package com.testwork.hotels.ui.first_fragment.adapter
 
 import androidx.recyclerview.widget.RecyclerView
 import com.testwork.domain.models.HotelDto
+import com.testwork.hotels.R
 import com.testwork.hotels.databinding.FirstFragmentFullItemBinding
 
 class HotelViewHolder(private val binding: FirstFragmentFullItemBinding) :
     RecyclerView.ViewHolder(binding.root) {
 
+    private val context = binding.root.context
+
     fun bind(item: HotelDto) {
         with(binding) {
             tvHotelName.text = item.name
+            tvHotelAddress.text = item.address
+
+            hotelMinPrice.text = context.getString(R.string.f_f_price, item.minimalPrice)
+            hotelMinPriceBy.text = item.priceForIt
+
+            ratingLayout.ratingText.text =
+                context.getString(R.string.f_f_rating, item.rating, item.ratingName)
+
             tvDescription.text = item.description
 
 
